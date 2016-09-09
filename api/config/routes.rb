f9b2594ad6a07fdef2837b3eae4e617e3e64a9ef
pedrocaseiro <pcaseiro96@gmail.com>
2016-09-09 09:37:00 +0100

    Add's Item logic

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     scope module: 'api' do
       namespace :v1 do
         resources :users
+        resources :items
       end
     end
   #end
---
+++ api/spec/factories/items.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :item do
+    name "MyString"
+    photo_url "MyString"
+  end
+end
