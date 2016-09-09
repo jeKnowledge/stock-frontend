02459e23de885c26f9f8601be3e0c2db8490e75f
Joao Bernardo <jb.amaro95@gmail.com>
2016-09-09 09:31:00 +0100

    Adds postgres database

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  constraints subdomain: 'api' do
+  #constraints subdomain: 'api' do
     scope module: 'api' do
       namespace :v1 do
-        resources :user
+        resources :users
       end
     end
-  end
+  #end
 end
