62d5edd26a2b860ef788cad16ffb8eb5b479863f
Joao Bernardo <jb.amaro95@gmail.com>
2016-09-09 15:58:44 +0100

    Remove counter route

--- frontend/src/components/Item/Item.js
+++ frontend/src/components/Item/Item.js
@@ -4,7 +4,7 @@ import classes from './Item.scss'
 export const Item = (props) => (
   <div className="col-xs-12 col-md-4">
     <div className="card">
-      <img className="card-img-top img-fluid" src={props.item.photoUrl} alt="Card image cap" />
+      <img className="card-img-top img-fluid" src={props.item.photo_url} alt="Card image cap" />
       <div className="card-block">
         <h4 className="card-title">{props.item.name}</h4>
         <a href="#" className="btn btn-primary">Info</a>
