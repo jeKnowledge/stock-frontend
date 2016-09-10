9bbe5c252df766256a8951c1dfe8180c648cf310
Joao Bernardo <jb.amaro95@gmail.com>
2016-09-10 19:32:53 +0100

    Adds stock logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import styles from './Header.scss'
 import HeaderLinks from 'components/HeaderLinks'
+import logo from './assets/logo.jpg'
 
 export const Header = () => (
   <nav className={ `navbar navbar-light bg-faded ${styles.navBar}` }>
     <div className="container">
       <IndexLink to='/' className="navbar-brand">
-        Stock
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="Logo" 
+        />
       </IndexLink>
 
       <HeaderLinks />
